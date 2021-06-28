decb4aa48b11e309a81648e0114618949d4c7a49
cougarhighland <xuan_dung.tran0001@stud.hkr.se>
2021-06-28 18:52:33 +0200

    exercises 1.3 -1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,25 +2,26 @@ import React from "react";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -34,11 +35,12 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  console.log(props);
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <p>{props.parts[0]["name"]} {props.parts[0]["exercises"]}</p>
+      <p>{props.parts[1]["name"]} {props.parts[1]["exercises"]}</p>
+      <p>{props.parts[2]["name"]} {props.parts[2]["exercises"]}</p>
     </div>
   );
 };
@@ -46,19 +48,10 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises}</p>
+      <p>Number of exercises {props.parts[0]["exercises"]+props.parts[1]["exercises"]+props.parts[2]["exercises"]}</p>
     </div>
   );
 };
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercise}
-      </p>
-    </div>
-  );
-};
 
 export default App;
